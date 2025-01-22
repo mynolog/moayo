26f526612b2a08aa8b643ba475b708398ca1fff6
mynolog <minholee.log@gmail.com>
2025-01-23 03:47:45 +0900

    init: frontend 초기 설정 구축

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <app-title />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppTitle from "@/components/AppTitle.vue";
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/AppTitle.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-4xl">{{ title }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const title = ref("moayo");
+</script>
+
+<style scoped></style>
