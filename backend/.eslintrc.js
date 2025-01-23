26093a207e354b37ca689a3c5bb42e1a6447a610
mynolog <minholee.log@gmail.com>
2025-01-24 00:42:50 +0900

    chore: backend - eslint, prettier 통합

---
+++ backend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        arrowParens: 'always',
+        printWidth: 100,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+};
