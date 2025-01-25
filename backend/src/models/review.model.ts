0bc06df8c34cc46a6d0e76e39ffbf3d6cdd799cd
mynolog <minholee.log@gmail.com>
2025-01-25 17:26:17 +0900

    feat: Book 컨트롤러, 라우트 연결 및 알라딘 API 도서 리스트 불러오기 기능 추가

--- backend/src/models/review.model.ts
+++ backend/src/models/review.model.ts
@@ -1,16 +1,18 @@
 import { Schema, model, Document } from 'mongoose';
 
-interface ReviewSchemaDocument extends Document {
+interface ReviewDocument extends Document {
   isbn: string;
   userId: string;
+  title: string;
   rating: number;
   content?: string;
 }
 
-const ReviewSchema = new Schema<ReviewSchemaDocument>(
+const ReviewSchema = new Schema<ReviewDocument>(
   {
     isbn: { type: String, required: true },
     userId: { type: String, required: true },
+    title: { type: String, required: true },
     rating: { type: Number, min: 1, max: 10, required: true },
     content: { type: String, required: false },
   },
