5661a4279663d6301e8a2c6f6363a604a6bb9c3d
mynolog <minholee.log@gmail.com>
2025-02-02 23:28:50 +0900

    refactor: backend - review 모델 title 키 제거 및 컨트롤러 수정 사항 반영

--- backend/src/models/review.model.ts
+++ backend/src/models/review.model.ts
@@ -3,18 +3,16 @@ import { Schema, model, Document } from 'mongoose';
 export interface ReviewDocument extends Document {
   isbn13: string;
   accountId: string;
-  title: string;
   rating: number;
-  content?: string;
+  content: string;
 }
 
 const ReviewSchema = new Schema<ReviewDocument>(
   {
     isbn13: { type: String, required: true },
     accountId: { type: String, required: true },
-    title: { type: String, required: true },
     rating: { type: Number, min: 1, max: 5, required: true },
-    content: { type: String, required: false },
+    content: { type: String, required: true },
   },
   { timestamps: true },
 );
